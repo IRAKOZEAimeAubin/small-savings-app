a0e29d1d541bf5d53e9f8502159f7b6f01da4088
Irakoze Aimé Aubin <irakozeaimeaubin@gmail.com>
2024-04-04 18:18:56 +0200

    commit #2

--- auth.config.ts
+++ auth.config.ts
@@ -1,7 +1,33 @@
-import GitHub from 'next-auth/providers/github'
-
+import bcrypt from 'bcryptjs'
 import type {NextAuthConfig} from 'next-auth'
+import Credentials from 'next-auth/providers/credentials'
+import Google from 'next-auth/providers/google'
+import {LoginSchema} from '@/schemas/user'
+import {getUserByEmail} from '@/data/auth/user'
 
 export default {
-  providers: [GitHub],
+  providers: [
+    Google({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    }),
+    Credentials({
+      async authorize(credentials) {
+        const validatedFields = LoginSchema.safeParse(credentials)
+
+        if (validatedFields.success) {
+          const {email, password} = validatedFields.data
+
+          const user = await getUserByEmail(email)
+          if (!user || !user.password) return null
+
+          const passwordMatch = await bcrypt.compare(password, user.password)
+
+          if (passwordMatch) return user
+        }
+
+        return null
+      },
+    }),
+  ],
 } satisfies NextAuthConfig
--- components/form/social.tsx
+++ components/form/social.tsx
@@ -1,16 +1,22 @@
 'use client'
 
+import {signIn} from 'next-auth/react'
 import {FcGoogle} from 'react-icons/fc'
 import {Button} from '@/components/ui/button'
+import {DEFAULT_LOGIN_REDIRECT} from '@/routes'
 
 export const Social = () => {
+  const onClick = (provider: 'google') => {
+    signIn(provider, {callbackUrl: DEFAULT_LOGIN_REDIRECT})
+  }
+
   return (
     <div className='flex items-center w-full gap-x-2'>
       <Button
         size='lg'
         className='w-full flex items-center gap-x-2'
         variant='outline'
-        onClick={() => {}}
+        onClick={() => onClick('google')}
       >
         <FcGoogle />
         Authenticate with Google
